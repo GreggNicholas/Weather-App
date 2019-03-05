d68a010f8b8b5684c8f18f572d2554f26b74490f
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-04 19:55:25 -0500

    Creates RetroFitSingleton

--- app/src/main/java/com/example/weather/Service/RetroFitSingleton.java
+++ app/src/main/java/com/example/weather/Service/RetroFitSingleton.java
@@ -1,4 +1,20 @@
 package com.example.weather.Service;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetroFitSingleton {
+    private static Retrofit instance;
+    private static final String BASE_URL = "http://api.aerisapi.com/";
+
+    public static Retrofit newInstance() {
+        if (instance == null) {
+            instance = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return instance;
+
+    }
+
 }
