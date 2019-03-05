ae4a971ae3450b26fb664ec71a37a168d6fc2b1b
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-04 20:16:23 -0500

    Adds DisplayForecastFragment

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.weather.Service.WeatherService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView weatherView = findViewById(R.id.text_show_forecast_main);
+        final Button weatherButton = findViewById(R.id.button);
+
+
+        weatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String getForecastResponse = weatherButton.getText().toString();
+
+
+                WeatherService weatherService =
+
+
+
+
+            }
+        });
+
+
     }
 }
