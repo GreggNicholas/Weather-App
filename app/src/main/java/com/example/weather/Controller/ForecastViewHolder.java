7b9e60d58a238b8198cd21590a2f18a598dd13cc
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-04 22:00:26 -0500

    Adds Recycler view changes

--- app/src/main/java/com/example/weather/Controller/ForecastViewHolder.java
+++ app/src/main/java/com/example/weather/Controller/ForecastViewHolder.java
@@ -3,14 +3,23 @@ package com.example.weather.Controller;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.weather.Model.WeatherModel;
+import com.example.weather.R;
 
 public class ForecastViewHolder extends RecyclerView.ViewHolder {
+    private ForecastAdapter adapter;
+    private TextView maxTemp;
+    private TextView minTemp;
+    private TextView dateAndTime;
+
     public ForecastViewHolder(@NonNull View itemView) {
         super(itemView);
     }
 
-    public void onBind(WeatherModel weatherModel) {
+
+    public void onBind(final WeatherModel weatherModel) {
+       dateAndTime = itemView.findViewById(R.id.weather_view);
     }
 }
--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.weather.Controller.ForecastAdapter;
 import com.example.weather.Fragment.DisplayForecastFragment;
 import com.example.weather.Model.WeatherListResponse;
+import com.example.weather.Model.WeatherModel;
 import com.example.weather.Service.RetroFitSingleton;
 import com.example.weather.Service.WeatherService;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +35,21 @@ public class MainActivity extends AppCompatActivity {
         final Retrofit retrofit = RetroFitSingleton.newInstance();
         WeatherService service = retrofit.create(WeatherService.class);
         Call<WeatherListResponse> weatherListResponseCall = service.getWeatherUpdates("dYNLXJfHo7ygPXu8cOhAA", "u42nGUhdZLwh1KQALowBVj5n34qFzRW1hY6DQ8jr");
+        weatherListResponseCall.enqueue(new Callback<WeatherListResponse>() {
+            @Override
+            public void onResponse(Call<WeatherListResponse> call, Response<WeatherListResponse> response) {
+                Log.d(TAG, "onResponse: " + response.body().getWeatherListResponse().get(0).getWeatherPeriods().get(0));
+                List<WeatherModel> weatherModelList = response.body().getWeatherListResponse().get(0).getWeatherPeriods().subList(0, 2);
 
+                recyclerView.setAdapter(new ForecastAdapter(weatherModelList));
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<WeatherListResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
+        });
 
 
     }
